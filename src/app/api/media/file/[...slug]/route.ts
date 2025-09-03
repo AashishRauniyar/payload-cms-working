c18b93171b3cc32d27e36c2a393d43a14a2a85d6
AashishRauniyar <rauniyaaraashish@gmail.com>
2025-09-03 16:29:50 +0545

    final i give up

--- src/app/api/media/file/[...slug]/route.ts
+++ src/app/api/media/file/[...slug]/route.ts
@@ -2,8 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import path from 'path'
 import fs from 'fs'
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string[] } }) {
+export async function GET(request: NextRequest, context: { params: Promise<{ slug: string[] }> }) {
   try {
+    const params = await context.params
     const filename = params.slug.join('/')
     const filePath = path.join(process.cwd(), 'public', 'media', filename)
 
