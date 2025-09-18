252c7f51e6f0d41033e541c8bc91a8eeba03bde6
AashishRauniyar <rauniyaaraashish@gmail.com>
2025-09-18 13:14:46 +0545

    FIX: migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250916_062336 from './20250916_062336';
+import * as migration_20250918_072824 from './20250918_072824';
 
 export const migrations = [
   {
     up: migration_20250916_062336.up,
     down: migration_20250916_062336.down,
-    name: '20250916_062336'
+    name: '20250916_062336',
+  },
+  {
+    up: migration_20250918_072824.up,
+    down: migration_20250918_072824.down,
+    name: '20250918_072824'
   },
 ];
